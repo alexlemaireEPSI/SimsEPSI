f0aaa66be7a88057cfc15bc1a56d37d236bf02f3
Lovix99 <61746502+Lovix99@users.noreply.github.com>
2020-03-05 17:16:20 +0100

    Travail.v1

--- src/main/java/com/mycompany/simsepsi/Travail.java
+++ src/main/java/com/mycompany/simsepsi/Travail.java
@@ -5,10 +5,48 @@
  */
 package com.mycompany.simsepsi;
 
-/**
- *
- * @author gauti
- */
+
 public class Travail {
-    
+
+	private String nomSim;
+	private String dateTravail;
+	
+	public Travail(String nom ,String date) {
+		// TODO Auto-generated method stub
+		Math.random();
+		nomSim = nom;
+		dateTravail = date;
+		System.out.println(date+" : "+nom+" va au travail.");
+		System.out.println(date+" : "+nom+" va manger.");
+		System.out.println(date+" : "+nom+" quitte le travail.");
+	}
+	
+	private void discute()
+	{
+		System.out.println(dateTravail+" : "+nomSim+" discute avec son collègue.");
+		System.out.println("Sociabilité +1");
+	}
+	
+	private void gateau()
+	{
+		System.out.println(dateTravail+" : "+nomSim+" trouve un mars sur son bureau.");
+		System.out.println("Faim +1");
+	}
+	
+	private void bourreauTravail()
+	{
+		System.out.println(dateTravail+" : "+nomSim+" a très bien travaillé aujourd'hui.");
+		System.out.println("Satisfaction Personnel +1");
+		System.out.println("Faim -1");
+		System.out.println("Energie -3");
+	}
+	
+	
+	private void patron()
+	{
+		System.out.println(dateTravail+" : "+nomSim+" est convoqué par le patron.");
+		System.out.println("Stress +1");
+		System.out.println(dateTravail+" : "+nomSim+" est rassuré, l'entretien s'est bien passé.");
+		System.out.println("Stress -5");
+	}
 }
