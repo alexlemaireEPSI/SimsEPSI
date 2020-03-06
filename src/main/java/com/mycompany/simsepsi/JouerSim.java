b6a0bfcf04aa9713707bc9cd1e798c6194fee1ec
Gautier Naudin <50101619+GautierNaudin@users.noreply.github.com>
2020-03-06 09:38:39 +0100

    Update JouerSim.java

--- src/main/java/com/mycompany/simsepsi/JouerSim.java
+++ src/main/java/com/mycompany/simsepsi/JouerSim.java
@@ -11,4 +11,23 @@ package com.mycompany.simsepsi;
  */
 public class JouerSim {
     
+    private static int jour = 1;
+    private static int heure = 6;
+    private static int minute = 0;
+    public static String newLine = System.getProperty("line.separator");
+    
+    public void Run()
+    {
+        Sim sim = new Sim(this);
+    }
+    
+    public void PasserTemps(int heure, int minute)
+    {
+        
+    }
+    
+    public void ReglerHeure(int heure, int minute)
+    {
+        
+    }
 }
